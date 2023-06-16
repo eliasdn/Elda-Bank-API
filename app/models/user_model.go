862f457b3c457e21afe91e71e7d1efc295af128f
eliasdn <79227843+eliasdn@users.noreply.github.com>
2023-06-16 22:47:31 +0200

    Update user_model.go

--- app/models/user_model.go
+++ app/models/user_model.go
@@ -8,11 +8,13 @@ import (
 
 // User struct to describe User object.
 type User struct {
-	ID           uuid.UUID `db:"id" json:"id" validate:"required,uuid"`
-	CreatedAt    time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt    time.Time `db:"updated_at" json:"updated_at"`
-	Email        string    `db:"email" json:"email" validate:"required,email,lte=255"`
-	PasswordHash string    `db:"password_hash" json:"password_hash,omitempty" validate:"required,lte=255"`
-	UserStatus   int       `db:"user_status" json:"user_status" validate:"required,len=1"`
-	UserRole     string    `db:"user_role" json:"user_role" validate:"required,lte=25"`
+	ID           uuid.UUID `gorm:"primaryKey;not null;unique" db:"id" json:"id" validate:"required,uuid"`
+	CreatedAt    time.Time `gorm:"not null" db:"created_at" json:"created_at"`
+	UpdatedAt    time.Time `gorm:"not null" db:"updated_at" json:"updated_at"`
+	Username     string    `gorm:"not null" db:"username" json:"username" validate:"required,lte=25"`
+	Email        string    `gorm:"not null" db:"email" json:"email" validate:"required,email,lte=255"`
+	PasswordHash string    `gorm:"not null" db:"password_hash" json:"password_hash" validate:"required,lte=255"`
+	UserStatus   int       `gorm:"not null" db:"user_status" json:"user_status" validate:"required,len=1"`
+	UserRole     string    `gorm:"not null" db:"user_role" json:"user_role" validate:"required,lte=6"`
+	Disabled     bool      `gorm:"notn ull,default:false"`
 }
