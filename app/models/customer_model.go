d830fa1bebb40215a4ef518c0aec3493f3a1e431
eliasdn <79227843+eliasdn@users.noreply.github.com>
2023-06-17 10:37:07 +0200

    import Customer model

--- app/models/customer_model.go
+++ app/models/customer_model.go
@@ -12,10 +12,10 @@ type Customer struct {
 	CreatedAt   time.Time `gorm:"not null" db:"created_at" json:"created_at"`
 	UpdatedAt   time.Time `gorm:"not null" db:"updated_at" json:"updated_at"`
 	FirstName   string    `gorm:"not null" db:"firstName" json:"firstName" validate:"required,lte=25"`
-	LastName    string    `gorm:"not null" db:"lastName" json:"lastName" validate:"required,email,lte=255"`
-	DOB         time.Time `gorm:"not null" db:"dob" json:"dob" validate:"required,lte=255"`
-	Address     string    `gorm:"not null" db:"address" json:"address" validate:"required,len=1"`
-	City        string    `gorm:"not null" db:"city" json:"city" validate:"required,lte=6"`
+	LastName    string    `gorm:"not null" db:"lastName" json:"lastName" validate:"required,email,lte=25"`
+	DOB         time.Time `gorm:"not null" db:"dob" json:"dob" validate:"required,lte=50"`
+	Address     string    `gorm:"not null" db:"address" json:"address" validate:"required,lte=80"`
+	City        string    `gorm:"not null" db:"city" json:"city" validate:"required,lte=35"`
 	PostalCode  int       `gorm:"notn ull" db:"postalCode" json:"postalCode" validate:"required,lte=9"`
 	NumberPhone int       `gorm:"not null" db:"number_phone" json:"number_phone" validate:"required,lte=15"`
 }
